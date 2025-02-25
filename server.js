8193c40d23679abb774ae67781c02c36e4e201f9
Arpit <arpitsengar99@gmail.com>
2025-02-25 13:48:34 +0530

    udpate code

--- server.js
+++ server.js
@@ -1,27 +1,70 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 app.use(express.static(__dirname));
+
+// Track rooms and their participants
+const rooms = new Map();
 
 io.on('connection', socket => {
     console.log('User connected:', socket.id);
 
-    socket.on('offer', offer => {
-        socket.broadcast.emit('offer', offer);
+    socket.on('join-room', (roomId) => {
+        // Leave previous room if any
+        Array.from(socket.rooms).forEach(room => {
+            if (room !== socket.id) socket.leave(room);
+        });
+
+        // Initialize room if it doesn't exist
+        if (!rooms.has(roomId)) {
+            rooms.set(roomId, new Set());
+        }
+
+        const room = rooms.get(roomId);
+        
+        // Join room
+        socket.join(roomId);
+        room.add(socket.id);
+        
+        // Notify others in the room about new participant
+        socket.to(roomId).emit('peer-joined', socket.id);
+        
+        // Send existing participants to the new peer
+        socket.emit('room-joined', {
+            roomId,
+            participants: Array.from(room).filter(id => id !== socket.id)
+        });
     });
 
-    socket.on('answer', answer => {
-        socket.broadcast.emit('answer', answer);
+    socket.on('offer', ({offer, roomId}) => {
+        socket.to(roomId).emit('offer', {offer, from: socket.id});
     });
 
-    socket.on('ice-candidate', candidate => {
-        socket.broadcast.emit('ice-candidate', candidate);
+    socket.on('answer', ({answer, roomId}) => {
+        socket.to(roomId).emit('answer', {answer, from: socket.id});
+    });
+
+    socket.on('ice-candidate', ({candidate, roomId}) => {
+        socket.to(roomId).emit('ice-candidate', {candidate, from: socket.id});
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
+        // Remove user from all rooms
+        rooms.forEach((participants, roomId) => {
+            participants.delete(socket.id);
+            if (participants.size === 0) {
+                rooms.delete(roomId);
+            } else {
+                socket.to(roomId).emit('peer-disconnected', socket.id);
+            }
+        });
     });
 });
 
